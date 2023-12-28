2be78ddd3a5e726ec4e67d1d116da001b1985459
Maria Z <z0559882272@gmail.com>
2023-12-28 20:57:14 +0200

    loginSuccess

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,39 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        //if SingOut present --->logout
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
 
     @Test
-    public void test(){
-        
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+//        Assert.assertEquals();
+//        Assert.assertTrue();
+//        Assert.assertNotEquals();
+//        Assert.assertFalse();
+
+    }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
