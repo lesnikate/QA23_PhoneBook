14f64b0456fb797950979f0748b16a6c0d1042a6
lesnikate <katerina.lesnikov@gmail.com>
2023-12-29 08:42:37 +0300

    LoginTest

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,32 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+
+    }
+
     @Test
-    public void test(){
-        
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("ret@het.com", "Qc$123456");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+//        Assert.assertEquals();
+//        Assert.assertTrue();
+//        Assert.assertNotEquals();
+//        Assert.assertFalse();
+
+
     }
 }
