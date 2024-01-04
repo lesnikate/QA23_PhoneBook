8d0046f3ba35257636ebe51da107810a239a5616
Maria Z <z0559882272@gmail.com>
2024-01-04 20:14:13 +0200

    login negative

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,8 +1,11 @@
 package manager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -13,24 +16,45 @@ public class HelperBase {
         this.wd = wd;
     }
 
-    public void type(By locator, String text){
+    public void type(By locator, String text) {
         WebElement element = wd.findElement(locator);
         element.click();
         element.clear();
-        if(text!=null){
-           element.sendKeys(text);
+        if (text != null) {
+            element.sendKeys(text);
         }
     }
 
 
-    public void click(By locator){
+    public void click(By locator) {
         WebElement element = wd.findElement(locator);
         element.click();
     }
 
-public boolean isElementPresent(By locator){
-    List<WebElement>list = wd.findElements(locator);
-    return list.size()>0;
-}
+    public boolean isElementPresent(By locator) {
+        List<WebElement> list = wd.findElements(locator);
+        return list.size() > 0;
+    }
 
+    public boolean isAlertPresent(String message) {
+        Alert alert = new WebDriverWait(wd, 10)
+                .until(ExpectedConditions.alertIsPresent());
+        if (alert != null && alert.getText().contains(message)) {
+            //click ok-->
+            //pause(5000);
+            alert.accept();
+            //click cancel--> alert.dismiss();
+            //type into alert--> alert.sendKeys("hello");
+            return true;
+        }
+        return false;
+    }
+
+    public void pause(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -36,4 +36,31 @@ public class LoginTests extends TestBase {
 
         Assert.assertTrue(app.getHelperUser().isLogged());
     }
+
+    @Test
+    public void loginWrongEmail(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("maragmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+    @Test
+    public void loginWrongPassword(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com", "Mmar123");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+
+
+    }
+    @Test
+    public void loginUnregisteredUser(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("luck@gmail.com", "Lluk123456$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+
+    }
+
+
 }
